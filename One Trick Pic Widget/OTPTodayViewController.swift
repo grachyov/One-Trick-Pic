9d16972d16373ba14277fa69bd43e34da1194976
IvanGrachyov <grachb@icloud.com>
2015-04-13 17:37:29 +0300

    New icon, little fixes.

--- One Trick Pic Widget/OTPTodayViewController.swift
+++ One Trick Pic Widget/OTPTodayViewController.swift
@@ -26,12 +26,6 @@ class OTPTodayViewController: UIViewController, NCWidgetProviding {
     }
 
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
-        // Perform any setup necessary in order to update the view.
-
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-
         completionHandler(NCUpdateResult.NewData)
     }
     
@@ -40,7 +34,7 @@ class OTPTodayViewController: UIViewController, NCWidgetProviding {
     }
     
     @IBAction func copyAndDeleteButtonPressed(sender: AnyObject) {
-        println("copyAndDeleteButtonPressed")
+        println("copyAndDeleteButtonPressedInWidget")
         imageManager.copyAndDeleteLastImage()
     }
     
--- OneTrickPicKit/OTPPhotosManager.swift
+++ OneTrickPicKit/OTPPhotosManager.swift
@@ -19,12 +19,12 @@ public class OTPPhotosManager {
         let fetchOptions = PHFetchOptions()
         fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
         let fetchResults = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions)
-        let lastAsset = fetchResults.lastObject as PHAsset
+        let lastAsset = fetchResults.lastObject as! PHAsset
         let imageDataRequestOptions = PHImageRequestOptions()
         imageDataRequestOptions.version = PHImageRequestOptionsVersion.Current
         PHImageManager.defaultManager().requestImageDataForAsset(lastAsset, options: imageDataRequestOptions) {
             (imageData: NSData!, dataUTI: String!, orientation: UIImageOrientation, info: [NSObject : AnyObject]!) -> Void in
-            if !(info[PHImageResultIsDegradedKey] as Bool) && imageData != nil {
+            if !(info[PHImageResultIsDegradedKey] as! Bool) && imageData != nil {
                 let image = UIImage(data: imageData)
                 UIPasteboard.generalPasteboard().image = image
                 PHPhotoLibrary.sharedPhotoLibrary().performChanges({PHAssetChangeRequest.deleteAssets([lastAsset])}) {(success: Bool, error: NSError!) -> Void in}
